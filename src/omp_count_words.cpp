1b847bef2f5f275bfc4c9c2ea836c382c9056816
jehanshah8 <jehan.shah8@gmail.com>
2022-11-30 17:51:36 -0500

    working file read w mmap

--- src/omp_count_words.cpp
+++ src/omp_count_words.cpp
@@ -5,10 +5,11 @@
 #include <fstream>
 #include <sstream>
 
+#include <stdio.h>
 #include <fcntl.h>
 #include <sys/stat.h>
-#include <sys/mman.h> 
-
+#include <sys/mman.h>
+#include <unistd.h>
 #include <omp.h>
 
 #include "../include/utils.hpp"
@@ -16,92 +17,123 @@
 // make -f Makefile.omp
 // ./omp_count_words files/small_test1.txt files/small_test2.txt omp_wc.txt > omp_out.txt
 
+size_t GetFileSize(const int fd); 
+void* MmapFileToRead(const int fd, const int file_size) ;
+
 int main(int argc, char *argv[])
 {
-    if (argc < 3) {
-        std::cout << "Usage: count_words <input file 1> ... <input file n> <output file>" << std::endl; 
+    if (argc < 3)
+    {
+        std::cout << "Usage: count_words <input file 1> ... <input file n> <output file>" << std::endl;
         exit(0);
-    }  
+    }
 
     std::cout << "OpenMP Execution" << std::endl;
 
-    // Get sys info such as number of processors 
-    int num_max_threads = omp_get_max_threads(); // max number of threads available 
+    // Get sys info such as number of processors
+    int num_max_threads = omp_get_max_threads(); // max number of threads available
     std::cout << "\nProgram Configuration" << std::endl;
     std::cout << "Number of threads available = " << num_max_threads << std::endl;
 
     // Echo arguments
-    std::cout << "\nInput file(s): " << std::endl; 
-    for (int i = 1; i < argc - 1; i++) {
-        std::cout << "  - " << argv[i] << std::endl; 
+    std::cout << "\nInput file(s): " << std::endl;
+    for (int i = 1; i < argc - 1; i++)
+    {
+        std::cout << "  - " << argv[i] << std::endl;
     }
 
-    std::string output_filename = argv[argc - 1]; 
-    std::cout << "\nOutput file: " << output_filename << std::endl; 
+    std::string output_filename = argv[argc - 1];
+    std::cout << "\nOutput file: " << output_filename << std::endl;
 
-    // Go over each file 
-    for (int i = 1; i < argc - 1; i++) 
+    // Go over each file
+    for (int i = 1; i < argc - 1; i++)
     {
-        std::cout << "\nStarting to count words for " << argv[i] << std::endl; 
-        
-        // Open file using mmap to map file to virtual mem 
+        std::cout << "\nStarting to count words for " << argv[i] << std::endl;
+
         // Open file
         int fd = open(argv[i], O_RDONLY);
-        if (fd == -1) {   
+        if (fd == -1) {
             std::cerr << "Unable to open file!" << std::endl;
             exit(1);
         }
-
+        
         // Get file size
-        struct stat file_stat;
-        int file_status = fstat (fd, &file_stat);
+        size_t file_size = GetFileSize(fd);
 
-        if (status < 0) {
-            close(fd);
-            std::cerr << "Unable to get file status!" << std::endl;
-            exit(1);
-        }
-        size_t file_size = file_stat.st_size;
+        // Open file using mmap to map file to virtual mem
+        char* file_buffer = (char*) MmapFileToRead(fd, file_size);
+        
 
-        // Map file to mempory as a string
-        std::string file_buffer = mmap(0, file_size, PROT_READ, MAP_SHARED, fd, 0);
-        if (file_buffer == MAP_FAILED) {
-	        close(fd);
-	        std::cerr << "Error mapping file to memory!" << std::endl;
-            exit(1);
-        }
+
 
         std::cout << file_buffer << std::endl;
+        
+
+
+
 
         // Unmap file and close it
         if (munmap(file_buffer, file_size) == -1) {
             std::cerr << "Error un-mapping file to memory!" << std::endl;
             exit(1);
         }
-        close(fd); 
+        close(fd);
 
-        std::cout << "Finished counting words for " << argv[i] << std::endl; 
+        std::cout << "Finished counting words for " << argv[i] << std::endl;
     }
 
-    // Create reader threads 
+
+
+
+    // Create reader threads
     // Use multiple threads to read one file at a time before moving on to the next file
 
+    // Each reader thread will read one line
 
-    // Each reader thread will read one line 
-    
-    // OLD WAY and put it into the buffer of its corresponding mapper 
-    
+    // OLD WAY and put it into the buffer of its corresponding mapper
+
     // num_readers = k * num_mappers, where k is an integer
     // There needs to be 1 lock for each mapper's queue
 
     // Each mapper will acquire a lock on the queue, pop the first line
-    
+
     // Mapper will then process the line and update a local hash map
 
-    // Each time 
-    
+    // Each time
+
     // Mappers need to know when all readers are done
     // Reducers need to know when all mappers are done
 
-    return 0; 
+    return 0;
+}
+
+
+size_t GetFileSize(const int fd) 
+{
+    // Get file size
+    struct stat file_stat;
+    int file_status = fstat(fd, &file_stat);
+    if (file_status < 0)
+    {
+        close(fd);
+        std::cerr << "Unable to get file status!" << std::endl;
+        exit(1);
+    }
+    return file_stat.st_size;
+}
+
+
+void* MmapFileToRead(const int fd, const int file_size) 
+{
+    
+
+    void* file_buffer = mmap(0, file_size, PROT_READ, MAP_SHARED, fd, 0);
+    if (file_buffer == MAP_FAILED)
+    {
+        close(fd);
+        std::cerr << "Error mapping file to memory!" << std::endl;
+        exit(1);
+    }
+
+    return file_buffer; 
 }
--- src/serial_count_words.cpp
+++ src/serial_count_words.cpp
@@ -13,86 +13,90 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc < 3) {
-        std::cout<<"Usage: count_words <input file 1> ... <input file n> <output file>"<< std::endl; 
+    if (argc < 3)
+    {
+        std::cout << "Usage: count_words <input file 1> ... <input file n> <output file>" << std::endl;
         exit(0);
-    }   
-
-    // Program configuration
-    std::cout<<"Serial Execution"<<std::endl;
-
-    // Echo arguments
-    std::cout<<"\nInput file(s): "<<std::endl; 
-    for (int i = 1; i < argc - 1; i++) {
-        std::cout<< "  - "<< argv[i]<<std::endl; 
     }
 
-    std::string output_filename = argv[argc - 1]; 
-    std::cout<<"\nOutput file: "<<output_filename<< std::endl; 
+    // Program configuration
+    std::cout << "Serial Execution" << std::endl;
+
+    // Echo arguments
+    std::cout << "\nInput file(s): " << std::endl;
+    for (int i = 1; i < argc - 1; i++)
+    {
+        std::cout << "  - " << argv[i] << std::endl;
+    }
+
+    std::string output_filename = argv[argc - 1];
+    std::cout << "\nOutput file: " << output_filename << std::endl;
 
     // Read files one by one and build a hash table to hold reduced data
-    std::unordered_map<std::string, int> word_counts; 
+    std::unordered_map<std::string, int> word_counts;
 
-    double runtime = -omp_get_wtime(); // Start timer 
-    
-    // Go over each file 
-    for (int i = 1; i < argc - 1; i++) 
+    double runtime = -omp_get_wtime(); // Start timer
+
+    // Go over each file
+    for (int i = 1; i < argc - 1; i++)
     {
-        std::cout << "\nStarting to count words for " << argv[i]<< std::endl; 
-        
-        std::ifstream in_file; 
+        std::cout << "\nStarting to count words for " << argv[i] << std::endl;
+
+        std::ifstream in_file;
         in_file.open(argv[i]);
 
         // Check if file was opened successfully
-        if (!in_file) {   
-            std::cerr << "Unable to open file!" <<std::endl;
+        if (!in_file)
+        {
+            std::cerr << "Unable to open file!" << std::endl;
             exit(1);
         }
 
         // Read lines from file
-        while (in_file) {
+        while (in_file)
+        {
             std::string line_buffer;
             std::getline(in_file, line_buffer);
             GetWordCountsFromLine(line_buffer, word_counts);
         }
 
-        in_file.close(); 
-        std::cout << "Finished counting words for " << argv[i]<< std::endl; 
+        in_file.close();
+        std::cout << "Finished counting words for " << argv[i] << std::endl;
     }
 
     // Write the word counts to file
-    WriteWordCountsToFile(word_counts, output_filename); 
-    
-    runtime += omp_get_wtime(); // Stop timer 
-    std::cout<<"\nSerial execution time "<< runtime <<"seconds"<<std::endl;
+    WriteWordCountsToFile(word_counts, output_filename);
 
-    return 0; 
+    runtime += omp_get_wtime(); // Stop timer
+    std::cout << "\nSerial execution time " << runtime << "seconds" << std::endl;
+
+    return 0;
 }
 
 /**
 
 /// @brief Reads a string and updates a hash map with the number of appearances for each word
-/// @param line_buffer 
-/// @param word_counts 
-void GetWordCountsFromLine(std::string& line_buffer, std::unordered_map<std::string, int>& word_counts) 
+/// @param line_buffer
+/// @param word_counts
+void GetWordCountsFromLine(std::string& line_buffer, std::unordered_map<std::string, int>& word_counts)
 {
     std::istringstream buffer_string_stream(line_buffer);
     std::string word;
-    while (buffer_string_stream >> word) {   
+    while (buffer_string_stream >> word) {
         // Update hash map
         UpdateWordCounts(word_counts, word);
     }
 }
 
 /// @brief Increments map entry for the given key
-/// @param word_counts 
-/// @param word 
+/// @param word_counts
+/// @param word
 void UpdateWordCounts(std::unordered_map<std::string, int>& word_counts, const std::string& word)
 {
     if (word_counts.find(word) == word_counts.end()) {
         word_counts.insert({word, 1});
     } else {
-        word_counts[word] += 1; 
+        word_counts[word] += 1;
     }
 }
 */
