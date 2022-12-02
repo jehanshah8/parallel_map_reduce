89ae4c50abeeb1d40007cc771d615d6edc6531d2
jehanshah8 <jehan.shah8@gmail.com>
2022-12-01 22:40:44 -0500

    format

--- include/utils.hpp
+++ include/utils.hpp
@@ -7,9 +7,9 @@
 #include <string>
 #include <unordered_map>
 
-void GetWordCountsFromLine(std::string& line_buffer, std::unordered_map<std::string, int>& word_counts); 
-void UpdateWordCounts(std::unordered_map<std::string, int>& word_counts, const std::string& word);
+void GetWordCountsFromLine(std::string &line_buffer, std::unordered_map<std::string, int> &word_counts);
+void UpdateWordCounts(std::unordered_map<std::string, int> &word_counts, const std::string &word);
 
-void WriteWordCountsToFile(const std::unordered_map<std::string, int>& word_counts, const std::string filename); 
+void WriteWordCountsToFile(const std::unordered_map<std::string, int> &word_counts, const std::string &filename);
 
 #endif /* UTILS_H_ */
